64c22ec2edfdcdd0a04d143f1f8143982086425f
pcoitinho <pcoitinho@gmail.com>
2022-10-15 19:55:25 -0300

    chore:refactored utilts

--- util/array.lua
+++ util/array.lua
@@ -1,30 +1,31 @@
-return {
-    --- Returns index of a value in a table
-    -- @param table the input table
-    -- @param value the value to compare
-    -- @return the index of the element of -1 if not found
-    index = function(table, value)
-        for i, v in ipairs(table) do
-            if v == value then
-                return i
-            end
-        end
-        return -1
-    end,
+local array = {}
 
-
-    --- Finds first element and index in a table where 
-    -- matching function resolves to true
-    -- example: find({1, 2, 3}, function(v) return v > 1 end) returns 2, 2
-    -- @param table the input table
-    -- @param fn the matching function, should return boolean 
-    -- @return index, element tuple or an -1 and an empty table if none found
-    find = function(table, fn)
-        for i, v in ipairs(table) do
-            if fn(v) then
-                return i, v
-            end
+--- Returns index of a value in a table
+-- @param table the input table
+-- @param value the value to compare
+-- @return the index of the element of -1 if not found
+function array.index(table, value)
+    for i, v in ipairs(table) do
+        if v == value then
+            return i
         end
-        return -1, {}
     end
-}
+    return -1
+end
+
+--- Finds first element and index in a table where
+-- matching function resolves to true
+-- example: find({1, 2, 3}, function(v) return v > 1 end) returns 2, 2
+-- @param table the input table
+-- @param fn the matching function, should return boolean
+-- @return index, element tuple or an -1 and an empty table if none found
+function array.find(table, fn)
+    for i, v in ipairs(table) do
+        if fn(v) then
+            return i, v
+        end
+    end
+    return -1, {}
+end
+
+return array
--- util/vector.lua
+++ util/vector.lua
@@ -1,44 +1,43 @@
 --- Vector
 -- Minimal vector functions
 -- @module Vector
-local vector = {
+local vector = {}
 
-    --- Gets the length of a vector
-    -- @param a
-    -- @param b
-    -- @return length
-    len = function(a, b)
-        return math.sqrt(a * a + b * b)
-    end,
+--- Gets the length of a vector
+-- @param a
+-- @param b
+-- @return length
+function vector.len(a, b)
+    return math.sqrt(a * a + b * b)
+end
 
-    --- Normalizes vector to unit, so that its length is 1
-    -- @param a
-    -- @param b
-    -- @return normalized a, b tuple
-    normalize = function(a, b)
-        local len = math.sqrt(a * a + b * b)
-        return a / len, b / len
-    end,
+--- Normalizes vector to unit, so that its length is 1
+-- @param a
+-- @param b
+-- @return normalized a, b tuple
+function vector.normalize(a, b)
+    local len = vector.len(a, b)
+    return a / len, b / len
+end
 
-    --- Dot product
-    -- @param a1
-    -- @param a2
-    -- @param b1
-    -- @param b2
-    -- @return scalar value
-    dot = function(a1, b1, a2, b2)
-        return a1 * a2 + b1 * b2
-    end,
+--- Dot product
+-- @param a1
+-- @param a2
+-- @param b1
+-- @param b2
+-- @return scalar value
+function vector.dot(a1, b1, a2, b2)
+    return a1 * a2 + b1 * b2
+end
 
-    --- Rotates a vector by a given angle (radians)
-    -- @param a
-    -- @param b
-    -- @param angle
-    -- @return a, b tuple of rotated vector
-    rotate = function(a, b, angle)
-        return a * math.cos(angle) - b * math.sin(angle),
-            a * math.sin(angle) + b * math.cos(angle)
-    end,
-}
+--- Rotates a vector by a given angle (radians)
+-- @param a
+-- @param b
+-- @param angle
+-- @return a, b tuple of rotated vector
+function vector.rotate(a, b, angle)
+    return a * math.cos(angle) - b * math.sin(angle),
+        a * math.sin(angle) + b * math.cos(angle)
+end
 
 return vector
